57936407c5d83d49e616be48f1a8b2c41f1d05c6
Lareta064 <lareta1@yandex.ru>
2024-12-13 23:07:03 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -47,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			});
 		});
 	}
+	
 	/*==================FORM ===================*/
 	const formItems = document.querySelectorAll(".form-item");
 
